2b4fd251e6e82ffe27606ca6781a13495c211be4
AbhinandanMishra-IT <mishra.ravi94@gmail.com>
2026-02-06 10:11:04 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+const sections = document.querySelectorAll('.section');
+
+window.addEventListener('scroll', () => {
+    sections.forEach(section => {
+        const top = section.getBoundingClientRect().top;
+        const screenHeight = window.innerHeight;
+
+        if (top < screenHeight - 100) {
+            section.classList.add('show');
+        }
+    });
+});
